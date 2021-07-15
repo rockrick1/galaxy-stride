f7525ba144d47f7d35de2ad21075f232f57e72b4
Henrique Cerquinho <henriquecerquinho@gmail.com>
2021-07-14 21:04:56 -0300

    attempts on making some shaders

--- projectiles/base_shot.gd
+++ projectiles/base_shot.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+onready var env_settings = preload("res://projectiles/enemy/glow_env.tres")
+
+func _ready():
+	var env = WorldEnvironment.new()
+	env.set_environment(env_settings)
+	add_child(env)
+
+func _process(_delta):
+	if $Sprite != null:
+		$Sprite.set_modulate(Color(1.1,1.1,1.1,1))
+
 
 func set_vars(pos, shooter, dir):
 	position = pos
