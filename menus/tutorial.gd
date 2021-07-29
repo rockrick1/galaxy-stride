f9169a6dcfdd162e42b4eba84a5d7226bfd7583a
Henrique Cerquinho <henriquecerquinho@gmail.com>
2021-07-29 14:08:20 -0300

    updated tutorial

--- menus/tutorial.gd
+++ menus/tutorial.gd
@@ -6,6 +6,11 @@ const num_pages = 5
 
 
 func _ready():
+	$ColorRect/Pages/Labels1.show()
+	$ColorRect/Pages/Labels2.hide()
+	$ColorRect/Pages/Labels3.hide()
+	$ColorRect/Pages/Labels4.hide()
+	$ColorRect/Pages/Labels5.hide()
 	$ColorRect/Pages.set_animation("1")
 	$ColorRect/Num.set_text(str(current_page)+"/"+str(num_pages))
 
@@ -13,14 +18,18 @@ func _ready():
 func _on_Next_pressed():
 	if current_page == num_pages:
 		return
+	$ColorRect/Pages.get_node("Labels"+str(current_page)).hide()
 	current_page += 1
+	$ColorRect/Pages.get_node("Labels"+str(current_page)).show()
 	$ColorRect/Pages.set_animation(str(current_page))
 	$ColorRect/Num.set_text(str(current_page)+"/"+str(num_pages))
 
 func _on_Prev_pressed():
 	if current_page == 1:
 		return
+	$ColorRect/Pages.get_node("Labels"+str(current_page)).hide()
 	current_page -= 1
+	$ColorRect/Pages.get_node("Labels"+str(current_page)).show()
 	$ColorRect/Pages.set_animation(str(current_page))
 	$ColorRect/Num.set_text(str(current_page)+"/"+str(num_pages))
 
